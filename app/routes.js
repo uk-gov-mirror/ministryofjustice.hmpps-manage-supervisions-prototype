0e3b6b38441ded921339833e3f122c3d80b0fc6d
kelliedesigner <kelliematheson@gmail.com>
2021-03-05 12:18:27 +0000

    Update folder structure

--- app/routes.js
+++ app/routes.js
@@ -33,14 +33,14 @@ router.post('/v6/attendance-add-6', function (req, res) {
   }
 })
 
-router.post('/v6/appointment-update-2', function (req, res) {
+router.post('/v6/session-update-2', function (req, res) {
 
   let rearrangesession = req.session.data['rearrange-session']
 
   if (rearrangesession === 'true') {
-    res.redirect('/v6/appointment-update-2')
+    res.redirect('/v6/session-update-2')
   } else {
-    res.redirect('/v6/appointment-update-5-cancel')
+    res.redirect('/v6/session-update-5-cancel')
   }
 })
 
@@ -104,25 +104,25 @@ router.post('/confirm-attendance/v2/attendance-add-2', function (req, res) {
     }
   })
   
-  router.post('/arrange-a-session/v4/appointment-add-4', function (req, res) {
+  router.post('/arrange-a-session/v4/session-add-4', function (req, res) {
   
     let appointmentnotes = req.session.data['appointment-notes']
   
     if (appointmentnotes === 'true') {
-      res.redirect('/arrange-a-session/v4/appointment-add-4')
+      res.redirect('/arrange-a-session/v4/session-add-4')
     } else {
-      res.redirect('/arrange-a-session/v4/appointment-add-5')
+      res.redirect('/arrange-a-session/v4/session-add-5')
     }
   })
 
-  router.post('/arrange-a-session/v4/appointment-update-4', function (req, res) {
+  router.post('/arrange-a-session/v4/session-update-4', function (req, res) {
   
     let updatenotes = req.session.data['update-notes']
   
     if (updatenotes === 'true') {
-      res.redirect('/arrange-a-session/v4/appointment-update-4')
+      res.redirect('/arrange-a-session/v4/session-update-4')
     } else {
-      res.redirect('/arrange-a-session/v4/appointment-update-5')
+      res.redirect('/arrange-a-session/v4/session-update-5')
     }
   })
   
