7c287c7f1e73647baa5bb2f89ec3cae3ca6e2924
Jake Benilov <benilov@gmail.com>
2021-03-08 23:17:59 +0000

    Auto-correct JS files using standardjs library

--- app/data/case-list-data.js
+++ app/data/case-list-data.js
@@ -1,35 +1,35 @@
 module.exports = {
-  "caseList": [
+  'caseList': [
     {
-      "name": "Dylan Adam Armstrong",
-      "link": "progress",
-      "status": [
-        "Previously known",
-        "Order ended 24 Nov 2016"
+      'name': 'Dylan Adam Armstrong',
+      'link': 'progress',
+      'status': [
+        'Previously known',
+        'Order ended 24 Nov 2016'
       ],
-      "type": "Community Order",
-      "risk": "Medium",
-      "nextAppointment": null
+      'type': 'Community Order',
+      'risk': 'Medium',
+      'nextAppointment': null
     },
     {
-      "name": "Džiugas Atraskevicius",
-      "link": "",
-      "status": [
-        "Current"
+      'name': 'Džiugas Atraskevicius',
+      'link': '',
+      'status': [
+        'Current'
       ],
-      "type": "CJA - Indeterminate Public Prot.",
-      "risk": "Medium",
-      "nextAppointment": "2020-12-07T09:30:00"
+      'type': 'CJA - Indeterminate Public Prot.',
+      'risk': 'Medium',
+      'nextAppointment': '2020-12-07T09:30:00'
     },
     {
-      "name": "Spencer Gill",
-      "link": "",
-      "status": [
-        "No record"
+      'name': 'Spencer Gill',
+      'link': '',
+      'status': [
+        'No record'
       ],
-      "type": "ORA Suspended Sentence Order",
-      "risk": "High",
-      "nextAppointment": null
+      'type': 'ORA Suspended Sentence Order',
+      'risk': 'High',
+      'nextAppointment': null
     }
   ]
 }
