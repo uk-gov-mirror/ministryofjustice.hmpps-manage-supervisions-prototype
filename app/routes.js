2a29e24762cbe973d37c7282daa36a2266dd2fa3
Jake Benilov <benilov@gmail.com>
2021-04-07 19:38:46 +0100

    Remove obsolete routes

--- app/routes.js
+++ app/routes.js
@@ -61,30 +61,6 @@ router.post('/confirm-attendance/attendance-add-7', function (req, res) {
   }
 })
 
-//  Iteration 6 - version b - Remove RAR
-router.post('/confirm-attendance/attendance-add-3b', function (req, res) {
-  let complybranch = req.session.data['comply']
-
-  if (complybranch === 'false') {
-    res.redirect('/confirm-attendance/attendance-add-3b')
-  }
-  if (complybranch === 'true') {
-    res.redirect('/confirm-attendance/attendance-add-5b-remove-RAR')
-  } else {
-    res.redirect('/confirm-attendance/attendance-add-4b')
-  }
-})
-
-router.post('/confirm-attendance/attendance-add-7b', function (req, res) {
-  let sessionnotes = req.session.data['session-notes']
-
-  if (sessionnotes === 'true') {
-    res.redirect('/confirm-attendance/attendance-add-7b')
-  } else {
-    res.redirect('/confirm-attendance/attendance-add-8')
-  }
-})
-
 router.post('/confirm-attendance/attendance-add-7', function (req, res) {
   let sessionnotes = req.session.data['session-notes']
 
