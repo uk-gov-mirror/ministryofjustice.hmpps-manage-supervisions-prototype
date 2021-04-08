70b34b1464fd18a55d9f263b6e3f63629beb98fb
Jake Benilov <benilov@gmail.com>
2021-04-08 22:14:26 +0100

    Get routing to work for the 'confirm attendance' flow
    This is possible if the wizard fork path can be a function, as well as a string.

--- app/utils/confirm-attendance-wizard-paths.js
+++ app/utils/confirm-attendance-wizard-paths.js
@@ -29,6 +29,33 @@ function confirmAttendanceWizardForks (req) {
   const sessionId = req.params.sessionId
 
   var forks = [
+    {
+      currentPath: `/confirm-attendance/${CRN}/${sessionId}/compliance`,
+      storedData: ['confirm-attendance', CRN, sessionId, 'did-service-user-comply'],
+      excludedValues: [],
+      forkPath: (value) => {
+        switch (value) {
+          case 'Absent':
+            return `/confirm-attendance/${CRN}/${sessionId}/absence-acceptable`
+          case 'No':
+            return `/confirm-attendance/${CRN}/${sessionId}/non-compliance-reason`
+          default:
+            return `/confirm-attendance/${CRN}/${sessionId}/rar-categories`
+        }
+      }
+    },
+    {
+      currentPath: `/confirm-attendance/${CRN}/${sessionId}/non-compliance-reason`,
+      storedData: ['confirm-attendance', CRN, sessionId, 'was-absence-acceptable'],
+      excludedValues: [],
+      forkPath: `/confirm-attendance/${CRN}/${sessionId}/rar-categories`
+    },
+    {
+      currentPath: `/confirm-attendance/${CRN}/${sessionId}/add-notes`,
+      storedData: ['confirm-attendance', CRN, sessionId, 'add-notes'],
+      values: ['No'],
+      forkPath: `/confirm-attendance/${CRN}/${sessionId}/check`
+    }
   ]
 
   return nextForkPath(forks, req)
