c3f1f3ab39cdcadc35efc9e137be81f01ee1a89a
Jake Benilov <benilov@gmail.com>
2021-04-08 17:24:45 +0100

    Add CRN to the 'confirm attendance' pages

--- app/routes/confirm-attendance.js
+++ app/routes/confirm-attendance.js
@@ -1,34 +1,39 @@
 module.exports = router => {
-  const CRN = 'J678910'
-
   router.get([
-    '/confirm-attendance/:view'
+    '/confirm-attendance/:CRN/:view'
   ], (req, res, next) => {
     const data = req.session.data
-    res.locals.CRN = CRN
+    res.locals.CRN = req.params.CRN
+    res.locals.case = data.cases.find(obj => {
+      return obj.CRN === req.params.CRN
+    })
     next()
   })
 
-  router.post('/confirm-attendance/notes', function (req, res) {
-    let shouldAddNotes = req.session.data['confirm-a-session'][CRN]['add-notes'] === 'Yes'
+  router.get('/confirm-attendance/:CRN/:view', function (req, res) {
+    res.render(`confirm-attendance/${req.params.view}`)
+  })
+
+  router.post('/confirm-attendance/:CRN/notes', function (req, res) {
+    let shouldAddNotes = req.session.data['confirm-a-session'][req.params.CRN]['add-notes'] === 'Yes'
 
     if (shouldAddNotes) {
-      res.redirect('/confirm-attendance/notes')
+      res.redirect(`/confirm-attendance/${req.params.CRN}/notes`)
     } else {
-      res.redirect('/confirm-attendance/check')
+      res.redirect(`/confirm-attendance/${req.params.CRN}/check`)
     }
   })
 
-  router.post('/confirm-attendance/non-compliance-reason', function (req, res) {
-    switch (req.session.data['confirm-a-session'][CRN]['did-service-user-comply']) {
+  router.post('/confirm-attendance/:CRN/non-compliance-reason', function (req, res) {
+    switch (req.session.data['confirm-a-session'][req.params.CRN]['did-service-user-comply']) {
       case 'Yes':
-        res.redirect('/confirm-attendance/rar-categories')
+        res.redirect(`/confirm-attendance/${req.params.CRN}/rar-categories`)
         break;
       case 'No':
-        res.redirect('/confirm-attendance/non-compliance-reason')
+        res.redirect(`/confirm-attendance/${req.params.CRN}/non-compliance-reason`)
         break;
       default:
-        res.redirect('/confirm-attendance/absence-acceptable')
+        res.redirect(`/confirm-attendance/${req.params.CRN}/absence-acceptable`)
     }
   })
 }
