6dd686d5c904fa065426d3de1ce2ff78ab9bfcb3
Jake Benilov <benilov@gmail.com>
2021-04-15 16:56:36 +0100

    Remove session defaults that are now unused

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,15 +2,6 @@ const path = require('path')
 const cases = require('./cases')
 const helpers = require(path.join(__dirname, '../../lib/helpers.js'))
 
-const confirmAttendanceDefaults = (map, su) => {
-  if (su.previousAppointment && su.previousAppointment.sessionId) {
-    map[su.CRN] = {
-      [su.previousAppointment.sessionId]: su.previousAppointment
-    }
-  }
-  return map
-}
-
 const contactHistoryDefaults = (map, su) => {
   if (su.contactHistory) {
     map[su.CRN] = su.contactHistory.reduce((o, contact) => ({ ...o, [contact.sessionId]: contact}), {})
@@ -27,7 +18,6 @@ module.exports = {
   'team-codes': ['C17ETE'],
   cases: cases,
   'communication': cases.reduce(contactHistoryDefaults, {}),
-  'confirm-attendance': cases.reduce(confirmAttendanceDefaults, {}),
   'arrange-a-session': {
     'J678910': {
       123: cases
