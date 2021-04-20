e0f01c24a8d7a6578c7b27bda3eee10623d66759
Jake Benilov <benilov@gmail.com>
2021-04-20 15:58:14 +0100

    Add Gary's data

--- app/data/cases.js
+++ app/data/cases.js
@@ -546,5 +546,412 @@ module.exports = [
         'sessionId': 123
       }
     ]
+  },
+  {
+    'serviceUserPersonalDetails': {
+      'name': 'Gary Hart',
+      'firstName': 'Gary',
+      'dateOfBirth': '1999-10-02',
+      'address': [
+        '94 Kendell Street',
+        'Sheffield',
+        'S2 9NL'
+      ],
+      'phone': '07700 900721',
+      'email': 'example@example.com',
+      'aliases': [],
+      'preferredLanguage': 'English',
+      'nationality': 'British',
+      'ethnicity': 'White: British',
+      'religion': 'None',
+      'gender': 'Male',
+      'sexualOrientation': 'Heterosexual',
+      'disabilitiesAndAdjustments': [],
+      'circumstances': {
+        'employment': 'Full-time employed',
+        'housingStatus': 'Living with parents',
+        'safeguardingIssues': []
+      }
+    },
+    'personalContacts': [
+      {
+        'name': 'Shelly Davis',
+        'relationship': 'Partner',
+        'address': [
+          '100 Drive Street',
+          'Flat 10',
+          'Sheffield',
+          'S23 5FW'
+        ]
+      },
+      {
+        'name': 'Bill and Sally Hart',
+        'relationship': 'Parents',
+        'address': [
+          '94 Kendell Street',
+          'Sheffield',
+          'S2 9NL'
+        ]
+      }
+    ],
+    'professionalContacts': [
+      {
+        'name': 'Rotherham',
+        'phone': '0114 7496 0681',
+        'email': 'example@example.com',
+        'provider': 'CPA South Yorkshire',
+        'localDeliveryUnit': 'Rotherham',
+        'team': 'Rotherham LMC',
+        'allocatedUntilDate': '2019-11-19'
+      }
+    ],
+    'PNC': 'BC34587',
+    'CRN': '493227',
+    'currentOrder': {
+      'type': 'ORA Suspended Sentence Order',
+      'description': 'Common assault and battery (Criminal Law Act/CJ and Public Order Act) - 10501',
+      'lengthInMonths': 12,
+      'progressInMonths': 6,
+      'startDate': '2020-10-19',
+      'endDate': helpers.sentenceEndDate({lengthInMonths: 12, startDate: '2020-10-19'}),
+      'offenceDate': '2020-01-02',
+      'convictionDate': '2020-03-03',
+      'court': "Sheffield Magistrates' Court",
+      'responsibleCourt': "Sheffield Magistrates' Court",
+      'requirements': {
+        'rar': {
+          'type': 'Rehabilitation Activity Requirement (RAR)',
+          'value': '15 days',
+          'lengthInDays': 15,
+          'progressInDays': 3
+        },
+        'fine': {
+          'type': 'Alcohol treatment requirement',
+          'value': null
+        }
+      },
+      'courtDocuments': [
+        {
+          'name': 'Pre-sentence report',
+          'lastUpdateDate': '2020-10-19'
+        },
+        {
+          'name': 'Order',
+          'lastUpdateDate': '2020-10-19'
+        },
+        {
+          'name': 'CPS Pack',
+          'lastUpdateDate': '2020-10-19'
+        },
+        {
+          'name': 'Previous convictions',
+          'lastUpdateDate': '2020-10-19'
+        }
+      ],
+      'personalDocuments': [
+        {
+          'name': 'Induction pack',
+          'lastUpdateDate': '2020-10-26'
+        }
+      ]
+    },
+    'previousOrders': [
+      {
+        'title': 'ORA Suspended Sentence Order (12 Months)',
+        'description': 'Using violence to secure entry (Criminal Law Act/CJ and Public Order Act) - 19563',
+        'endDate': '2019-08-15'
+      }
+    ],
+    'breachesCount': 0,
+    'restrainingOrdersCount': 0,
+    'riskBadges': [
+      {
+        text: 'Risk to known adults',
+        class: 'red'
+      }
+    ],
+    'riskOfSeriousHarmLevel': {
+      text: 'Medium',
+      class: 'orange'
+    },
+    'riskOfHarm': [
+      {
+        'riskTo': 'Themselves',
+        'inCommunity': {
+          text: 'Medium',
+          class: 'orange'
+        },
+        'inCustody': {
+          text: 'High',
+          class: 'red'
+        }
+      },
+      {
+        'riskTo': 'Children',
+        'inCommunity': {
+          text: 'Medium',
+          class: 'orange'
+        },
+        'inCustody': {
+          text: 'High',
+          class: 'red'
+        }
+      },
+      {
+        'riskTo': 'Public',
+        'inCommunity': {
+          text: 'Medium',
+          class: 'orange'
+        },
+        'inCustody': {
+          text: 'High',
+          class: 'red'
+        }
+      },
+      {
+        'riskTo': 'Known adult',
+        'inCommunity': {
+          text: 'Medium',
+          class: 'orange'
+        },
+        'inCustody': {
+          text: 'High',
+          class: 'red'
+        }
+      },
+      {
+        'riskTo': 'Staff',
+        'inCommunity': {
+          text: 'Medium',
+          class: 'orange'
+        },
+        'inCustody': {
+          text: 'High',
+          class: 'red'
+        }
+      },
+      {
+        'riskTo': 'Prisoners',
+        'inCommunity': {
+          text: 'Medium',
+          class: 'orange'
+        },
+        'inCustody': {
+          text: 'High',
+          class: 'red'
+        }
+      }
+    ],
+    'criminogenicNeeds': [
+      'Alcohol Misuse',
+      'Relationships',
+      'Thinking and attitudes'
+    ],
+    'status': 'Previously known',
+    'previousOrderStatus': 'Order ended 15 Aug 2019',
+    'appointmentStatistics': {
+      'complied': 6,
+      'acceptableAbsence': 0,
+      'failureToComply': 0
+    },
+    'contactHistory': [
+      {
+        'type': 'Appointment',
+        'lastUpdatedBy': 'Mark Berridge',
+        'timestamp': helpers.today({ atTime: '10:00' }),
+        'session-date': helpers.today(),
+        'session-start-time': '10am',
+        'session-end-time': '11am',
+        'session-counts-towards-rar': 'No',
+        'type-of-session': 'Office visit',
+        'repeating': 'No, it’s a one-off session',
+        'confirmed': true,
+        'session-notes': `• Update Gary on referral to group programme
+        • Review alcohol diary`,
+        'sessionId': 10
+      },
+      {
+        'type': 'Appointment',
+        'timestamp': '2021-02-11T10:00',
+        'session-date': '2021-02-11',
+        'session-start-time': '10am',
+        'session-end-time': '11am',
+        'session-counts-towards-rar': 'Yes',
+        'session-rar-category': 'ES - RAR Programme',
+        'session-rar-subcategory': 'TSP (Thinking Skills)',
+        'type-of-session': 'Office visit',
+        'repeating': 'No, it’s a one-off appointment',
+        'confirmed': true,
+        'did-service-user-comply': 'Yes',
+        'lastUpdatedBy': 'Mark Berridge',
+        'session-notes': `Gary has managed to pay off his gambling debt sooner than he expected by taking on some overtime at work. He seems really delighted with the progress. We discussed methods on how to not accumulate debt from now on and that his next goal was to start saving for a flat.
+
+        His alcohol consumption has reduced however he still struggles when out with his mates. We moved on to another are to help focus on this by
+
+        • demonstrating improved emotional self management
+
+        His diary is a good step towards this but we discussed additional methods. He mentioned he likes going to his local gym, so we set a time early on Saturday mornings for him to book in a gym session so he avoids drinking too much the night before. We agreed to try this for a few weeks to see if it helps with his decision making.`,
+        'sessionId': 9
+      },
+      {
+        'type': 'Appointment',
+        'lastUpdatedBy': 'Mark Berridge',
+        'timestamp': '2021-01-11T10:00',
+        'session-date': '2021-01-11',
+        'session-start-time': '10am',
+        'session-end-time': '11am',
+        'session-counts-towards-rar': 'No',
+        'type-of-session': 'Office visit',
+        'did-service-user-comply': 'Yes',
+        'repeating': 'No, it’s a one-off session',
+        'confirmed': true,
+        'session-notes': `Gary arrived on time and presented well. There was no sign of recent alcohol use and we discussed his alcohol diary to which he has said he managed it 4 days out of 7. This is an improvement for him.
+
+        We discussed that I felt he would really benefit from group work on Building Better Relationships and he agreed he felt ready for group work now as he wanted to talk to other people “like him”
+
+        Our next meeting we shall review his alcohol diary once more and continue work on thinking skills and attitudes`,
+        'sessionId': 8
+      },
+      {
+        'type': 'Phone call',
+        'from': 'Service user',
+        'to': 'Mark Berridge',
+        'timestamp': '2021-01-04T13:00',
+        'contents': `Phone call from Gary to double check when his next appointment was, we talked briefly about how his Christmas and new year had been. He mentioned that he got quite drunk and had an argument with his ex partner Shelly over the phone. He admitted that he had gone to her home and shouted outside the door and the police were called but Shelly refused to press charges. This matched the Reportable incidents information I received this morning.
+
+        We talked about his goal to reduce his alcohol intake and that he will now keep a diary of his drinking and agreed to book in a supervision appointment for a week's time.`,
+        'sessionId': 7
+      },
+      {
+        'type': 'Appointment',
+        'timestamp': '2020-12-23T10:00',
+        'session-date': '2020-12-23',
+        'session-start-time': '10am',
+        'session-end-time': '11am',
+        'session-counts-towards-rar': 'Yes',
+        'session-rar-category': 'ES - RAR Programme',
+        'session-rar-subcategory': 'TSP (Thinking Skills)',
+        'type-of-session': 'Office visit',
+        'repeating': 'No, it’s a one-off appointment',
+        'confirmed': true,
+        'did-service-user-comply': 'Yes',
+        'lastUpdatedBy': 'Mark Berridge',
+        'session-notes': `Gary mentioned that his mum had helped him with his homework prior to the session. He was worried this would mean he had “cheated”. We discussed how leaning on others for support was a good thing when trying to complete a task especially someone like his mum who he has a good relationship with.
+
+          Gary has completed the tasks set for him at our previous meeting and has set out 3 goals for the next 6 months
+
+          Reduce his alcohol intake from 8 beers a day to 2
+          To start saving for a deposit on his own flat
+          To pay off his gambling debt of £800
+
+          We discussed techniques for each off those goals and agreed we will review them every month to monitor progress. We also discussed how his 2nd goal needed to have something we can measure success by and have agreed to change it to “save £500 towards the first months rent as deposit on a flat”
+
+          I have set Gary some further work for him to write down how he is going to achieve these things and break them down into steps`,
+        'sessionId': 6
+      },
+      {
+        'type': 'Appointment',
+        'timestamp': '2020-12-04T10:00',
+        'session-date': '2020-12-04',
+        'session-start-time': '10am',
+        'session-end-time': '11am',
+        'session-counts-towards-rar': 'Yes',
+        'session-rar-category': 'ES - RAR Programme',
+        'session-rar-subcategory': 'TSP (Thinking Skills)',
+        'type-of-session': 'Office visit',
+        'repeating': 'No, it’s a one-off appointment',
+        'confirmed': true,
+        'did-service-user-comply': 'Yes',
+        'lastUpdatedBy': 'Mark Berridge',
+        'session-notes': `Gary presented on time and engaged well with the session.
+
+        Started working with Gary on Thinking Skills and attitudes and agreed that our aim is to cover the following areas over the next few session
+
+        • demonstrating improved problem-solving and goal setting skills
+        • demonstrating improved emotional self management
+        • considering other people’s perspectives and emotional responses
+        • develop more positive relationships
+
+        This session we discussed problem solving and goal setting skills. I have set Gary some homework to set 5 things he would like to achieve over the next 6 months that will improve his well being. For review at our next session. `,
+        'sessionId': 5
+      },
+      {
+        'lastUpdatedBy': 'Mark Berridge',
+        'type': 'Email',
+        'to': 'someone@socialservices.gov.uk',
+        'from': 'Mark Berridge',
+        'timestamp': '2020-11-24T13:00',
+        'contents': `SUBJECT: Gary Hart
+        Hi there,
+
+        Just to inform you that Gary Hart is now longer living with his partner and her daughter which has reduced concerns I had over his living arrangements
+
+        I’ll update you if that changes in the future.`,
+        'sessionId': 4
+      },
+      {
+        'type': 'Appointment',
+        'lastUpdatedBy': 'Mark Berridge',
+        'timestamp': '2020-11-23T10:00',
+        'session-date': '2020-11-23',
+        'session-start-time': '10am',
+        'session-end-time': '11am',
+        'session-counts-towards-rar': 'No',
+        'type-of-session': 'Office visit',
+        'did-service-user-comply': 'Yes',
+        'repeating': 'No, it’s a one-off session',
+        'confirmed': true,
+        'session-notes': `Carried out initial sentence planning supervision appointments with Gary
+
+          Gary and I discussed some of his criminogenic needs, he agreed that those were three areas he needed support with especially his use of alcohol when dealing with relationships. We agreed an action to refer him to an alcohol treatment programmer however he also agreed that we should do some work together on thinking skills and attitudes.
+
+          Plan to allocate 5 of his 20 RAR days to that piece of work
+
+          His current relationship with his partner Shelly has ended since our last meeting and he has moved in with his parents, with whom he has a good relationship. He understands that this is a good opportunity to get his alcohol consumption under control in a supportive environment. However he is adamant that he does not want to do any group work. I believe that group work is important to him rehabilitating and reducing his chance of reoffending so will approach this with him in the future.
+
+          First 1-1 RAR session to be booked for 2 weeks time`,
+        'sessionId': 3
+      },
+      {
+        'type': 'Appointment',
+        'lastUpdatedBy': 'Mark Berridge',
+        'timestamp': '2020-10-24T10:00',
+        'session-date': '2020-10-24',
+        'session-start-time': '10am',
+        'session-end-time': '11am',
+        'session-counts-towards-rar': 'No',
+        'type-of-session': 'Office visit',
+        'did-service-user-comply': 'Yes',
+        'repeating': 'No, it’s a one-off session',
+        'confirmed': true,
+        'session-notes': `Induction appointment with Gary
+
+        Gary has always been in stable accommodation. Prior to moving in with his current partner he lived with his parents with whom he has a good relationship.
+
+        Gary has a stable employment history working as a labourer on a construction site with his father. He earns approx. £2,500 a month. He has around £900 debt as a result of gambling, but he is working towards paying this off.
+
+        Gary reports that he mostly drinks on weekends with his friends at the local pub. His previous conviction in 2019 was committed whilst he had been drinking heavily throughout the day.
+        Gary has no children.
+
+        Gary reports no issues with drug misuse. Gary reports good physical and mental health. There is no history of self-harm or suicide.`,
+        'sessionId': 2
+      },
+      {
+        'lastUpdatedBy': 'Mark Berridge',
+        'type': 'Email',
+        'from': 'someone@socialservices.gov',
+        'to': 'Mark Berridge',
+        'timestamp': '2020-10-23T13:00',
+        'contents': `Hi there,
+        I got your message about Gary Hart. Yes he is known to us.
+
+        He has previously threatened his partner and their daughter when under the influence of alcohol. On one occasion Mr Hart assaulted his daughter, on another occasion the daughter witnessed Mr Hart self-harming.
+
+        Daughter’s details follows:
+        Carina Hart
+        born 13/03/2009`,
+        'sessionId': 1
+      }
+    ]
   }
 ]
